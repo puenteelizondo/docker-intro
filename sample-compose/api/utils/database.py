25789251db3df9c3f1ca9f6df84d39026b34f1cf
jpuente10 <puenteelizondo02@gmail.com>
2023-07-17 07:38:13 -0600

    funcionando bien dockerfile y aparte tambien docker-compose

---
+++ sample-compose/api/utils/database.py
@@ -0,0 +1,20 @@
+import os
+
+# para crear una base de datos asincrona
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# creamos las variables de ambiente
+# tenemos que pasar las variables de ambiente de docker-compose a aquiiiiiiiiii
+MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+
+
+async def get_db():
+    # levantamos la base de datos asincrona
+    client = AsyncIOMotorClient(
+        # lo hacemos de esta manera la f es para concatenar
+        f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@db:27017"
+    )
+    return client.academia
--- sample_api/api/utils/database.py
+++ sample_api/api/utils/database.py
@@ -7,7 +7,8 @@ from pymongo.server_api import ServerApi
 async def get_db():
     # levantamos la base de datos asincrona
     client = AsyncIOMotorClient(
-                                #ip del contenedor
-        "mongodb://academia:academia@172.17.0.2:27017"
+        # ip del contenedor
+        # se tiene que cambiar
+        "mongodb://academia:academia@172.17.0.3:27017"
     )
     return client.academia
