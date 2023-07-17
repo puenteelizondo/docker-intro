81abaa08e61eb677dadd31ef279c8e5a8891624e
jpuente10 <puenteelizondo02@gmail.com>
2023-07-17 12:09:23 -0600

    funcionando correctamente con produccion y prueba completada con servicio de base de datos en la nube

--- sample-compose/api/utils/database.py
+++ sample-compose/api/utils/database.py
@@ -15,6 +15,7 @@ async def get_db():
     # levantamos la base de datos asincrona
     client = AsyncIOMotorClient(
         # lo hacemos de esta manera la f es para concatenar
+        #                                                           le pasamos la ip del servicio para que no alla problemas
         f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@db:27017"
     )
     return client.academia
